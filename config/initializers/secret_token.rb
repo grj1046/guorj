4bf50e4bdf0235b4c87fb921ea64d4e99450678d
grj <guorenjun@outlook.com>
2014-02-23 22:47:52 +0800

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guorj::Application.config.secret_key_base = '7a82355fc97c894e8054539c4697e50103318a6e07b8fbf9fbf754331e76967d6b5e7d28fadbc5edaf43687082d7bd87358c3a9cc62cb709b6a169ba77886a6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Guorj::Application.config.session_store :cookie_store, key: '_guorj_session'
