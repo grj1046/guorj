f9783d73057c2d177a58f4949184d9aabf25a59d
grj <guorenjun@outlook.com>
2014-03-05 21:41:14 +0800

    解决生产环境静态文件404的问题

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Guorj
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    #config.i18n.default_locale = :cn
+    config.assets.precompile += %w{ *.css *.js *.png *.jpg *.jpeg *.gif }
+    config.assets.initialize_on_precompile = false
+    config.enforce_avaliables_locales = true
   end
 end
