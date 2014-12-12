146008ba45dce8046d265fc011020d92d2ba1103
grj1046 <guorenjun@outlook.com>
2014-12-12 18:07:53 +0800

    重构闪存页面布局

--- app/controllers/site_configs_controller.rb
+++ app/controllers/site_configs_controller.rb
@@ -1,4 +1,5 @@
 class SiteConfigsController < ApplicationController
+  before_action :set_site_config, only: [:show, :edit, :update]
   before_action :signed_in_user
   before_action :admin_user
 
@@ -6,21 +7,25 @@ class SiteConfigsController < ApplicationController
     @site_configs = SiteConfig.all
   end
 
+  def show
+  end
+
   def edit
-    @site_config = SiteConfig.find(params[:id])
   end
 
   def update
-    @site_config = SiteConfig.find(params[:id])
-
     if @site_config.update_attributes(params[:site_config].permit!)
-      redirect_to edit_site_config_path(params[:id]), notice: '保存成功.'
+      redirect_to site_config_path(params[:id]), notice: '保存成功.'
     else
       render acton: 'edit'
     end
   end
 
   private
+    def set_site_config
+      @site_config = SiteConfig.find(params[:id])
+    end
+
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,16 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def notice_message
+    flash_message = []
+
+    flash.each do |type, message|
+      type = :success if type.to_sym == :notice
+      text = content_tag(:div, message, :class => "control-label has-#{type}")
+      flash_message << text if message
+    end
+ 
+    content_tag(:div, link_to('x', '#', :class => 'close', 'data-dismiss' => 'alert') + flash_message.join('\n').html_safe, :class => 'carousel flash') unless flash_message.empty?
+  end
 end
